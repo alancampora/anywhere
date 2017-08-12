24632a9dbf400ab955da3a2d678af0778bb322e1
ricardogcolombo <ricardogcolombo@gmail.com>
2017-08-13 01:52:22 +0200

    [ADD]Geolocation

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,49 +1,71 @@
 <template>
 
     <div class="results">
-        <card v-for="result in results" 
+        <card v-for="result in results"
             :key="result.permanent_id"
             :departure="result.departure_date"
-            :arrivalPlace="result.arrival_place.city_name"> 
-          </card>
+            :arrivalPlace="result.arrival_place.city_name">
+        </card>
     </div>
 </template>
 
 <script>
-    import blabla from '../services/blabla'
-    import Card from "./Card.vue"
+import blabla from '../services/blabla'
+import Card from "./Card.vue"
 
-    export default{
-        name: "Results",
-        components:{ Card }, 
-        data(){
-            return{
-                query: "",
-                results: [], 
-                isLoading: false
-            }
-        }, 
-        methods:{
-            search(){
-                this.isLoading = true;
-                blabla.search(this.query)
-                    .then(res =>
-                    {
-                        console.log(res)
-                        this.results = res.trips
-                        this.isLoading = false
-                    })
-            } 
-        }, 
-        created(){
-            this.search()            
+export default{
+    name: "Results",
+    components:{ Card },
+    data(){
+        return{
+            query: "",
+            results: [],
+            isLoading: false
         }
-    } 
+    },
+    methods:{
+
+        search(){
+            this.isLoading = true;
+
+            var geolocationOptions = {
+                enableHighAccuracy: false,
+                timeout: 10000,
+                maximumAge:Infinity
+            };
+
+            var getPosition = function (options) {
+                return new Promise(function (resolve, reject) {
+                    navigator.geolocation.getCurrentPosition(resolve, reject, geolocationOptions);
+                });
+            }
+
+            getPosition()
+                .then((position) => {
+                    blabla.search(position.coords)
+                        .then(res =>
+                            {
+                                console.log(res)
+                                this.results = res.trips
+                                this.isLoading = false
+                            })
+                })
+                .catch((err) => {
+                    //TODO send message error
+                    console.error(err.message);
+                });
+
+        }
+    },
+    created(){
+        this.search()
+    }
+}
 </script>
 
 <style>
 .results{
-    width: 100%; 
+    width: 100%;
     height: 100%;
 }
 </style>
--- src/services/blabla.js
+++ src/services/blabla.js
@@ -1,11 +1,19 @@
 const blablaService = {
-  baseUrl: "https://public-api.blablacar.com/api/v2/trips?fn=Heidelberg&key=bec1eafb73de4b77a3934f0b7088d674"
+  baseUrl: "https://public-api.blablacar.com/api/v2/trips?",
+  key: "key=bec1eafb73de4b77a3934f0b7088d674",
+  fc: "fc="
 }
 
-blablaService.search = function (query, type) {
-  const url = `${this.baseUrl}`
+blablaService.search = function(query, type) {
+  let url = `${this.baseUrl}`
 
-  return fetch(url, { method: 'GET' })
+  // add latitude | longitude
+  url += blablaService.fc + encodeURI(query.latitude + '|' + query.longitude) + "&" + blablaService.key;
+
+
+  return fetch(url, {
+      method: 'GET'
+    })
     .then(res => {
       if (res.status !== 200) {
         throw res.statusText
