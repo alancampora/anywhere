68f1f22dd57ad1b529eb60d53a729256cf87d6ee
ricardogcolombo <ricardogcolombo@gmail.com>
2017-08-14 00:15:39 +0200

    [FIX]set loader as first layer

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,27 +1,35 @@
-<template lang="html"> 
-    <div class="loader"> </div>
-</template> 
+<template lang="html">
+    <div class="loader-container">
+        <div class="loader"> </div>
+    </div>
+</template>
 
-<script> 
-    export default {
-        name: "Loader"
-    }
-</script> 
+<script>
+export default {
+    name: "Loader"
+}
+</script>
 
 <style scoped>
+.loader-container{
+    position:absolute;
+    width:100%;
+    height:100%;
+    z-index:3;
+}
 .loader {
-  position: fixed;
-  left: 50%;
-  top: 50%;
-  z-index: 1;
-  margin: -75px 0 0 -75px;
-  border: 16px solid rgba(243, 243, 243, 0.29);
-  border-radius: 50%;
-  border-top: 16px solid #3498db;
-  width: 100px;
-  height: 100px;
-  -webkit-animation: spin 2s linear infinite;
-  animation: spin 2s linear infinite;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    z-index:4;
+    margin: -75px 0 0 -75px;
+    border: 16px solid rgba(243, 243, 243, 0.29);
+    border-radius: 50%;
+    border-top: 16px solid #3498db;
+    width: 100px;
+    height: 100px;
+    -webkit-animation: spin 2s linear infinite;
+    animation: spin 2s linear infinite;
 }
 @keyframes spin {
     0% { transform: rotate(0deg); }
