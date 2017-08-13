ac1441358816f7290b1909744158416c9387eb8e
Alan Campora <alan.campora@gmail.com>
2017-08-13 21:53:43 +0200

    [MOD] changing api_key

--- src/services/flickr.js
+++ src/services/flickr.js
@@ -1,7 +1,7 @@
 const flickrService = {
   baseUrl: "https://api.flickr.com/services/rest/?",
   authToken: "auth_token=72157685013754423-a975daf6a06c36a7&",
-  key:"api_key=fd07938471f41428fa07137bf81cfae2",
+  key:"api_key=195f9e6bfd8621d296c7852591b97a54&",
   methods: {
       photoSearch:"method=flickr.photos.search",
       getInfo:"method=flickr.photos.getInfo&"
