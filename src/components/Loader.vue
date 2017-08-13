08d499394da084c4b6bf299f72066140ce5d980b
ricardogcolombo <ricardogcolombo@gmail.com>
2017-08-14 00:19:49 +0200

    [FIX]loader styles

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -16,6 +16,8 @@ export default {
     width:100%;
     height:100%;
     z-index:3;
+    top:0;
+    background-color:rgba(3,3,3,0.2);
 }
 .loader {
     position: fixed;
@@ -23,9 +25,9 @@ export default {
     top: 50%;
     z-index:4;
     margin: -75px 0 0 -75px;
-    border: 16px solid rgba(243, 243, 243, 0.29);
+    border: 16px solid rgba(172, 197, 92, 0.29);
     border-radius: 50%;
-    border-top: 16px solid #3498db;
+    border-top: 16px solid #acc55c;
     width: 100px;
     height: 100px;
     -webkit-animation: spin 2s linear infinite;
